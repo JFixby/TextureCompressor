8e01acda5a30fbdde79c82ebf14ce26095cda570
J Fixby <github@jfixby.com>
2016-03-24 18:13:02 +0100

    +test

---
+++ jfixby-tool-texture-compressor-test/src/com/jfixby/tool/texturecompressor/test/IndexImageTest.java
@@ -0,0 +1,76 @@
+package com.jfixby.tool.texturecompressor.test;
+
+import java.io.IOException;
+
+import com.jfixby.cmns.api.color.Colors;
+import com.jfixby.cmns.api.color.GraySet;
+import com.jfixby.cmns.api.desktop.ImageAWT;
+import com.jfixby.cmns.api.file.File;
+import com.jfixby.cmns.api.file.LocalFileSystem;
+import com.jfixby.cmns.api.image.ArrayColorMap;
+import com.jfixby.cmns.api.image.ColorMap;
+import com.jfixby.cmns.api.image.ColorMapSpecs;
+import com.jfixby.cmns.api.image.GrayIndexedλImage;
+import com.jfixby.cmns.api.image.GrayMap;
+import com.jfixby.cmns.api.image.ImageProcessing;
+import com.jfixby.red.desktop.DesktopAssembler;
+import com.jfixby.tools.gdx.texturepacker.api.indexed.IndexedCompressor;
+import com.jfixby.tools.texturepacker.red.indexed.RedIndexedCompressor;
+
+public class IndexImageTest {
+
+    public static void main(String[] args) throws IOException {
+	DesktopAssembler.setup();
+	IndexedCompressor.installComponent(new RedIndexedCompressor());
+
+	File home = LocalFileSystem.ApplicationHome();
+	File input_folder = home.child("input");
+	File output_folder = home.child("indexed");
+	File originalFile = input_folder.child("fox.png");
+	final ArrayColorMap image = ImageAWT.readAWTColorMap(originalFile);
+
+	GrayMap red = image.getRed();
+	GrayMap green = image.getGreen();
+	GrayMap blue = image.getBlue();
+	GrayMap alpha = image.getAlpha();
+
+	// 0.21 R + 0.72 G + 0.07 B
+	int N = 16;
+	GraySet red_palette = Colors.newUniformGraySet((int) (-(0f - 0.21) * N));
+	GraySet green_palette = Colors.newUniformGraySet((int) (-(0f - 0.72) * N));
+	GraySet blue_palette = Colors.newUniformGraySet((int) (-2 * (0f - 0.07) * N));
+	GraySet alpha_palette = Colors.newUniformGraySet((int) (-(0f - 0.07) * N));
+
+	red_palette.print("red");
+	green_palette.print("green");
+	blue_palette.print("blue");
+	alpha_palette.print("alpha");
+
+	int W = image.getWidth();
+	int H = image.getHeight();
+
+	GrayIndexedλImage red_indexed = ImageProcessing.index(red, red_palette);
+	GrayIndexedλImage green_indexed = ImageProcessing.index(green, green_palette);
+	GrayIndexedλImage blue_indexed = ImageProcessing.index(blue, blue_palette);
+	GrayIndexedλImage alpha_indexed = ImageProcessing.index(alpha, alpha_palette);
+
+	red = ImageProcessing.newGrayMap(red_indexed, W, H);
+	green = ImageProcessing.newGrayMap(green_indexed, W, H);
+	blue = ImageProcessing.newGrayMap(blue_indexed, W, H);
+	alpha = ImageProcessing.newGrayMap(alpha_indexed, W, H);
+
+	{
+	    ColorMapSpecs specs = ImageProcessing.newColorMapSpecs();
+	    specs.setColorMapDimentions(W, H);
+	    specs.setRed(red);
+	    specs.setGreen(green);
+	    specs.setBlue(blue);
+	    specs.setAlpha(alpha);
+	    ColorMap colormap = ImageProcessing.newColorMap(specs);
+
+	    ImageAWT.writeToFile(colormap, output_folder.child("indexed.png"), "png");
+	}
+
+    }
+
+}
