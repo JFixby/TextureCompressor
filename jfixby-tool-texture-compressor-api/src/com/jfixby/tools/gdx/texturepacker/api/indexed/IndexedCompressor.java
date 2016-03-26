36dc0940fc4fe061875ff9d92260b8273a5596f9
J Fixby <github@jfixby.com>
2016-03-26 19:46:21 +0100

    +alpha compression

--- jfixby-tool-texture-compressor-api/src/com/jfixby/tools/gdx/texturepacker/api/indexed/IndexedCompressor.java
+++ jfixby-tool-texture-compressor-api/src/com/jfixby/tools/gdx/texturepacker/api/indexed/IndexedCompressor.java
@@ -10,9 +10,10 @@ public class IndexedCompressor {
 
     public static final String INDEXED_COLOR_FILE_EXTENTION = ".r3-icolor";
 
-    public static final int DEFAULT_RED_PALETTE_SIZE = 16 * 2;
-    public static final int DEFAULT_GREEN_PALETTE_SIZE = 32 * 2;
-    public static final int DEFAULT_BLUE_PALETTE_SIZE = 16 * 2;
+    public static final int DEFAULT_RED_PALETTE_SIZE = 32;
+    public static final int DEFAULT_GREEN_PALETTE_SIZE = 64;
+    public static final int DEFAULT_BLUE_PALETTE_SIZE = 32;
+    public static final int DEFAULT_ALPHA_PALETTE_SIZE = 128;
 
     static private ComponentInstaller<IndexedCompressorComponent> componentInstaller = new ComponentInstaller<IndexedCompressorComponent>(
 	    "IndexedCompressor");
