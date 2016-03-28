ef11c696e8df6a2d951b7c7448a4639d2afa8a38
J Fixby <github@jfixby.com>
2016-03-28 22:53:22 +0200

    +compression strategy option flag

--- jfixby-tool-texture-compressor-api/src/com/jfixby/tools/gdx/texturepacker/api/indexed/CompressionStrategy.java
+++ jfixby-tool-texture-compressor-api/src/com/jfixby/tools/gdx/texturepacker/api/indexed/CompressionStrategy.java
@@ -2,13 +2,32 @@ package com.jfixby.tools.gdx.texturepacker.api.indexed;
 
 public enum CompressionStrategy {
 
-    SEPARATE_CHANNELS, MERGE_CHANNELS;
+    SEPARATE_CHANNELS(53), MERGE_CHANNELS(31);
+
+    private byte byteFlag;
+
+    CompressionStrategy(int byteFlag) {
+	this.byteFlag = (byte) byteFlag;
+    }
 
     public static CompressionStrategy checkDefault(CompressionStrategy compressionStrategy) {
 	if (compressionStrategy != null) {
 	    return compressionStrategy;
 	}
 	return SEPARATE_CHANNELS;
+    }
+
+    public byte byteFlag() {
+	return byteFlag;
+    }
+
+    public static CompressionStrategy valueOf(byte readByte) {
+	for (int i = 0; i < values().length; i++) {
+	    if (values()[i].byteFlag == readByte) {
+		return values()[i];
+	    }
+	}
+	return null;
     }
 
 }
--- jfixby-tool-texture-compressor-test/src/com/jfixby/tool/texturecompressor/test/HistogrammTest.java
+++ jfixby-tool-texture-compressor-test/src/com/jfixby/tool/texturecompressor/test/HistogrammTest.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 import com.jfixby.cmns.api.collections.Collections;
 import com.jfixby.cmns.api.collections.Histogramm;
 import com.jfixby.cmns.api.color.Color;
+import com.jfixby.cmns.api.color.ColorProjector;
+import com.jfixby.cmns.api.color.Colors;
+import com.jfixby.cmns.api.color.GraySet;
 import com.jfixby.cmns.api.desktop.ImageAWT;
 import com.jfixby.cmns.api.file.File;
 import com.jfixby.cmns.api.file.LocalFileSystem;
@@ -30,18 +33,42 @@ public class HistogrammTest {
 
 	// File compressedFile =
 	// output_folder.child("index_compressed.r3-icolor");
-	final Histogramm<Float> reds = Collections.newHistogramm();
-	final Histogramm<Float> greens = Collections.newHistogramm();
-	final Histogramm<Float> blues = Collections.newHistogramm();
-//	final Histogramm<Float> alphas = Collections.newHistogramm();
+	final Histogramm<Integer> reds = Collections.newHistogramm();
+	final Histogramm<Integer> greens = Collections.newHistogramm();
+	final Histogramm<Integer> blues = Collections.newHistogramm();
+	// final Histogramm<Float> alphas = Collections.newHistogramm();
 	final ArrayColorMap image = ImageAWT.readAWTColorMap(originalFile);
+
+	final GraySet green_palette = Colors.newUniformGraySet(IndexedCompressor.DEFAULT_GREEN_PALETTE_SIZE);
+	final GraySet red_palette = Colors.newUniformGraySet(IndexedCompressor.DEFAULT_RED_PALETTE_SIZE/2);
+	final GraySet blue_palette = Colors.newUniformGraySet(IndexedCompressor.DEFAULT_BLUE_PALETTE_SIZE/2);
+
+	final ColorProjector projector = new ColorProjector() {
+
+	    @Override
+	    public Color findClosest(Color other) {
+		Color color = Colors.newColor(//
+			red_palette.findClosest(other.red()), //
+			green_palette.findClosest(other.green()), //
+			blue_palette.findClosest(other.blue())//
+		);
+		// L.d("" + other, color);
+		return color;
+	    }
+	};
+
 	for (int i = 0; i < image.getWidth(); i++) {
 	    for (int j = 0; j < image.getHeight(); j++) {
-		Color color = image.valueAt(i, j);
-		reds.addIf(color.red() * 255, color.red() > 0 && color.red() < 1);
-		greens.addIf(color.green() * 255, color.green() > 0 && color.green() < 1);
-		blues.addIf(color.blue() * 255, color.blue() > 0 && color.blue() < 1);
-//		alphas.addIf(color.alpha() * 255, color.alpha() > 0 && color.alpha() < 1);
+		Color color = (image.valueAt(i, j));
+		float red = red_palette.findClosest(color.red()); //
+		float green = green_palette.findClosest(color.green()); //
+		float blue = blue_palette.findClosest(color.blue());//
+
+		reds.add(ImageProcessing.roundArgument(red * 255));
+		greens.add(ImageProcessing.roundArgument(green * 255));
+		blues.add(ImageProcessing.roundArgument(blue * 255));
+		// alphas.addIf(color.alpha() * 255, color.alpha() > 0 &&
+		// color.alpha() < 1);
 	    }
 	}
 	reds.sortValues();
@@ -53,13 +80,13 @@ public class HistogrammTest {
 	blues.sortValues();
 	blues.print("blues");
 
-//	alphas.sort();
-//	alphas.print("alphas");
+	// alphas.sort();
+	// alphas.print("alphas");
 
 	GrayλImage redImage = histogrammImage(reds);
 	GrayλImage greenImage = histogrammImage(greens);
 	GrayλImage blueImage = histogrammImage(blues);
-//	GrayλImage alphaImage = histogrammImage(alphas);
+	// GrayλImage alphaImage = histogrammImage(alphas);
 
 	{
 	    ColorMapSpecs specs = ImageProcessing.newColorMapSpecs();
@@ -98,25 +125,28 @@ public class HistogrammTest {
 	    ImageAWT.writeToFile(colormap, output_folder.child("histogramm-blue.png"), "png");
 	}
 	{
-//	    ColorMapSpecs specs = ImageProcessing.newColorMapSpecs();
-//	    specs.setColorMapDimentions(256, 256);
-//	    specs.setRed(alphaImage);
-//	    specs.setGreen(alphaImage);
-//	    specs.setBlue(alphaImage);
-//	    ColorMap colormap = ImageProcessing.newColorMap(specs);
-//
-//	    colormap = ImageProcessing.newColorMap(ImageProcessing.scale(colormap, 2, 2), 256 * 2, 256 * 2);
-//
-//	    ImageAWT.writeToFile(colormap, output_folder.child("histogramm-alpha.png"), "png");
+	    // ColorMapSpecs specs = ImageProcessing.newColorMapSpecs();
+	    // specs.setColorMapDimentions(256, 256);
+	    // specs.setRed(alphaImage);
+	    // specs.setGreen(alphaImage);
+	    // specs.setBlue(alphaImage);
+	    // ColorMap colormap = ImageProcessing.newColorMap(specs);
+	    //
+	    // colormap =
+	    // ImageProcessing.newColorMap(ImageProcessing.scale(colormap, 2,
+	    // 2), 256 * 2, 256 * 2);
+	    //
+	    // ImageAWT.writeToFile(colormap,
+	    // output_folder.child("histogramm-alpha.png"), "png");
 	}
 
     }
 
-    private static GrayλImage histogrammImage(final Histogramm<Float> histogramm) {
+    private static GrayλImage histogrammImage(final Histogramm<Integer> histogramm) {
 	return new GrayλImage() {
 	    @Override
 	    public float valueAt(float x, float y) {
-		long num = histogramm.getNumberOf(1f * ImageProcessing.roundArgument(x));
+		long num = histogramm.getNumberOf(ImageProcessing.roundArgument(x));
 		long max = histogramm.getMax();
 		float value = 1f - (num * 1f / max);
 		// float value = (num * 1f / max);
