d5417ea86acc0e2f65a98c708eb8b08e5083ce08
J Fixby <github@jfixby.com>
2016-03-31 12:44:07 +0200

    ...

--- jfixby-tool-texture-compressor-test/src/com/jfixby/tool/texturecompressor/test/IndexImageTest.java
+++ jfixby-tool-texture-compressor-test/src/com/jfixby/tool/texturecompressor/test/IndexImageTest.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import com.jfixby.cmns.api.file.File;
 import com.jfixby.cmns.api.file.LocalFileSystem;
+import com.jfixby.cmns.api.log.L;
 import com.jfixby.red.desktop.DesktopAssembler;
 import com.jfixby.tools.gdx.texturepacker.api.indexed.IndexedCompressor;
 import com.jfixby.tools.texturepacker.red.indexed.RedIndexedCompressor;
@@ -18,8 +19,11 @@ public class IndexImageTest {
 	File input_folder = home.child("input");
 	File output_folder = home.child("indexed");
 	File originalFile = input_folder.child("fox.png");
+	File processing = output_folder.child("index.png");
 
-	IndexedCompressor.indexImage(originalFile, output_folder.child("indexed.png"));
+	home.getFileSystem().copyFileToFile(originalFile, processing);
+	L.d("indexing", processing);
+	IndexedCompressor.indexImage(processing, processing);
 
     }
 
