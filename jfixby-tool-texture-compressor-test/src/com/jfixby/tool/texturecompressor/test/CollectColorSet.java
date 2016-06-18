603c89cb8d3aaddfbbd77cbb719606ef84fb40f2
J Fixby <github@jfixby.com>
2016-06-18 23:51:46 +0200

    format code

--- jfixby-tool-texture-compressor-test/src/com/jfixby/tool/texturecompressor/test/CollectColorSet.java
+++ jfixby-tool-texture-compressor-test/src/com/jfixby/tool/texturecompressor/test/CollectColorSet.java
@@ -1,3 +1,4 @@
+
 package com.jfixby.tool.texturecompressor.test;
 
 import java.io.IOException;
@@ -18,32 +19,32 @@ import com.jfixby.tools.texturepacker.red.indexed.RedIndexedCompressor;
 
 public class CollectColorSet {
 
-    public static void main(String[] args) throws IOException {
-	DesktopSetup.deploy();
-	IndexedCompressor.installComponent(new RedIndexedCompressor());
+	public static void main (final String[] args) throws IOException {
+		DesktopSetup.deploy();
+		IndexedCompressor.installComponent(new RedIndexedCompressor());
 
-	File home = LocalFileSystem.ApplicationHome();
-	File input_folder = home.child("input");
-	File output_folder = home.child("indexed");
-//	File originalFile = input_folder.child("hks-e.png");
-	File originalFile = input_folder.child("fox.png");
-	final ArrayColorMap image = ImageAWT.readAWTColorMap(originalFile);
+		final File home = LocalFileSystem.ApplicationHome();
+		final File input_folder = home.child("input");
+		final File output_folder = home.child("indexed");
+// File originalFile = input_folder.child("hks-e.png");
+		final File originalFile = input_folder.child("fox.png");
+		final ArrayColorMap image = ImageAWT.readAWTColorMap(originalFile);
 
-	final ColorSet palette = Colors.newColorsSet(image);
+		final ColorSet palette = Colors.newColorsSet(image);
 
-	palette.print(originalFile.getName());
+		palette.print(originalFile.getName());
 
-	ColoredλImage paletteImage = new ColoredλImage() {
-	    @Override
-	    public Color valueAt(float x, float y) {
-		return palette.getElementAt((int) x);
-	    }
-	};
+		ColoredλImage paletteImage = new ColoredλImage() {
+			@Override
+			public Color valueAt (final float x, final float y) {
+				return palette.getElementAt((int)x);
+			}
+		};
 
-	paletteImage = ImageProcessing.scale(paletteImage, 8, 8);
-	ColorMap colors = ImageProcessing.newColorMap(paletteImage, palette.size() * 8, palette.size() * 2);
-	ImageAWT.writeToFile(colors, output_folder.child("palette.png"), "png");
+		paletteImage = ImageProcessing.scale(paletteImage, 8, 8);
+		final ColorMap colors = ImageProcessing.newColorMap(paletteImage, palette.size() * 8, palette.size() * 2);
+		ImageAWT.writeToFile(colors, output_folder.child("palette.png"), "png");
 
-    }
+	}
 
 }
