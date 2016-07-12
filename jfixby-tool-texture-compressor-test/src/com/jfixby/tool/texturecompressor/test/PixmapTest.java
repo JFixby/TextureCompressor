78ca08daaabc684930a0fd99c698e2955b28e1d2
J Fixby <github@jfixby.com>
2016-07-12 20:36:34 +0200

    ...

--- jfixby-tool-texture-compressor-test/src/com/jfixby/tool/texturecompressor/test/PixmapTest.java
+++ jfixby-tool-texture-compressor-test/src/com/jfixby/tool/texturecompressor/test/PixmapTest.java
@@ -1,3 +1,4 @@
+
 package com.jfixby.tool.texturecompressor.test;
 
 import com.badlogic.gdx.files.FileHandle;
@@ -8,26 +9,26 @@ import com.badlogic.gdx.utils.GdxNativesLoader;
 
 public class PixmapTest {
 
-    public static void main(String[] args) {
-	
-	GdxNativesLoader.load();
-	Pixmap.setBlending(Pixmap.Blending.None);
-	final float W = 400;
-	final float H = 400;
-	Pixmap pixmap = new Pixmap((int) W, (int) H, Format.RGBA8888);
-	pixmap.setColor(0x0000FF00);// clear background, Alpha == 0
-	pixmap.fill();
-	for (int x = 0; x < W; x++) {
-	    for (int y = 0; y < H; y++) {
-		pixmap.setColor(0xffffff80);// set color White with Alpha=0.5
-		pixmap.drawPixel(x, y);
-	    }
-	}
-	FileHandle file = new FileHandle("1.png");
-	System.out.println("writing: " + file);
-	PixmapIO.writePNG(file, pixmap);
-	pixmap.dispose();
+	public static void main (final String[] args) {
 
-    }
+		GdxNativesLoader.load();
+		Pixmap.setBlending(Pixmap.Blending.None);
+		final float W = 400;
+		final float H = 400;
+		final Pixmap pixmap = new Pixmap((int)W, (int)H, Format.RGBA8888);
+		pixmap.setColor(0x0000FF00);// clear background, Alpha == 0
+		pixmap.fill();
+		for (int x = 0; x < W; x++) {
+			for (int y = 0; y < H; y++) {
+				pixmap.setColor(0xffffff80);// set color White with Alpha=0.5
+				pixmap.drawPixel(x, y);
+			}
+		}
+		final FileHandle file = new FileHandle("1.png");
+		System.out.println("writing: " + file);
+		PixmapIO.writePNG(file, pixmap);
+		pixmap.dispose();
+
+	}
 
 }
